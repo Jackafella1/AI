1e94e79445f183e62e81954199dc76a99df54c9b
Jackafella1 <jackafella@gmail.com>
2025-02-25 12:30:27 +0000

    crawler added

---
+++ chatbot/src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Header from "@/components/Header";
+import Chat from "@/components/Chat";
+import { useChat } from "ai/react";
+
+const Page: React.FC = () => {
+  const [context, setContext] = useState<string[] | null>(null);
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div className="...">
+      <Header className="..." />
+      <div className="...">
+        <Chat
+          input={input}
+          handleInputChange={handleInputChange}
+          handleMessageSubmit={handleSubmit}
+          messages={messages}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
